6e934e30fc386b62ff5e933c5b59e05afbfbfdb1
Mohamed Seed <mosaeed7888@gmail.com>
2025-02-28 10:12:02 +0200

    Update axiosClient to use NEXT_PUBLIC_REST_API_KEY from environment variables

--- e-commerce/app/(utils)/axiosClient.js
+++ e-commerce/app/(utils)/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const apiKey=process.env.REST_API_KEY;
+const apiKey=process.env.NEXT_PUBLIC_REST_API_KEY;
 const apiUrl='https://e-commerce-strapi-railway-production.up.railway.app/api'//products
+console.log("API Key in axiosClient:", apiKey);
 
 const axiosClient = axios.create({
 	baseURL: apiUrl,
