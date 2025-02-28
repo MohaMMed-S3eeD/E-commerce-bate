961056d5898710f273d7e5e4a24cc983930c8df7
Mohamed Seed <mosaeed7888@gmail.com>
2025-02-28 09:42:29 +0200

    Update axiosClient to use REST_API_KEY from environment variables

--- e-commerce/app/(utils)/axiosClient.js
+++ e-commerce/app/(utils)/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey=process.env.NEXT_PUBLIC_REST_API_KEY;
+const apiKey=process.env.REST_API_KEY;
 const apiUrl='https://e-commerce-strapi-railway-production.up.railway.app/api'//products
 
 const axiosClient = axios.create({
